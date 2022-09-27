077da8d322c6e60ca61b070e14c36c0f3e418ce0
Quentin Hello <9997584+qhello@users.noreply.github.com>
2022-09-27 13:54:06 +0200

    fix(getContractById): fix logic to allow contract to be queried by either ContractorId or ClientId

--- src/controllers/getContractById.js
+++ src/controllers/getContractById.js
@@ -1,8 +1,16 @@
+const { Op } = require("sequelize");
+
 module.exports = async (req, res) => {
-    const { Contract } = req.app.get("models");
-    const { id } = req.params;
-    const { id: ContractorId } = req.profile;
-    const contract = await Contract.findOne({ where: { id, ContractorId } });
-    if (!contract) return res.status(404).end();
-    res.json(contract);
-  }
+  const { Contract } = req.app.get("models");
+  const { id } = req.params;
+  const { id: profileId } = req.profile;
+  const contract = await Contract.findOne({
+    where: {
+      id,
+      [Op.or]: [{ ContractorId: profileId }, { ClientId: profileId }],
+    },
+  });
+  if (!contract) return res.status(404).end();
+
+  res.json(contract);
+};
--- src/controllers/getContractById.spec.js
+++ src/controllers/getContractById.spec.js
@@ -9,14 +9,23 @@ describe("getContractById", () => {
     expect(res.body).toMatchSnapshot();
   });
 
-  it("should return 404 if contract doesn't belong to profile", async () => {
+  it("should return contract", async () => {
     const res = await request(app).get("/contracts/1").set("profile_id", 1);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toMatchSnapshot();
+  });
+
+  it("should return 404 if contract doesn't belong to profile", async () => {
+    const res = await request(app).get("/contracts/1").set("profile_id", 2);
 
     expect(res.statusCode).toEqual(404);
   });
 
   it("should return 404 if contract doesn't exists", async () => {
-    const res = await request(app).get("/contracts/foobar").set("profile_id", 5);
+    const res = await request(app)
+      .get("/contracts/foobar")
+      .set("profile_id", 5);
 
     expect(res.statusCode).toEqual(404);
   });
