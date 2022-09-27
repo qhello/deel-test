6bc8931d4238941816f683c00999a87dd7a2c3ff
Quentin Hello <9997584+qhello@users.noreply.github.com>
2022-09-27 13:45:20 +0200

    chore(getContractById): add test to getContractById

---
+++ src/controllers/getContractById.spec.js
@@ -0,0 +1,23 @@
+const request = require("supertest");
+const app = require("../app");
+
+describe("getContractById", () => {
+  it("should return contract", async () => {
+    const res = await request(app).get("/contracts/1").set("profile_id", 5);
+
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toMatchSnapshot();
+  });
+
+  it("should return 404 if contract doesn't belong to profile", async () => {
+    const res = await request(app).get("/contracts/1").set("profile_id", 1);
+
+    expect(res.statusCode).toEqual(404);
+  });
+
+  it("should return 404 if contract doesn't exists", async () => {
+    const res = await request(app).get("/contracts/foobar").set("profile_id", 5);
+
+    expect(res.statusCode).toEqual(404);
+  });
+});
